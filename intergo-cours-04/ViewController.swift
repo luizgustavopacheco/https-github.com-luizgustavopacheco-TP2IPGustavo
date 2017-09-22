9bc446ffa8c90d242b5bda25b26ff454518af0bd
eleves <eleves@mac204-05.local>
2017-09-22 14:22:37 -0400

    Initial Commit

---
+++ intergo-cours-04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  intergo-cours-04
+//
+//  Created by eleves on 17-09-22.
+//  Copyright © 2017 eleves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
